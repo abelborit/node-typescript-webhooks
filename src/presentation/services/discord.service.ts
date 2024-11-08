4aef8cc7827beac382b8d3014128692654c6a094
Abel Borit <abelboritguitton@gmail.com>
2024-11-08 17:08:40 -0500

    [add] send image to Discord

--- src/presentation/services/discord.service.ts
+++ src/presentation/services/discord.service.ts
@@ -8,6 +8,13 @@ export class DiscordService {
   async notify(message: string) {
     const body = {
       content: message,
+      // embeds: [
+      //   {
+      //     image: {
+      //       url: "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMncxMTZseDAwM2RpdzN6d2w1aXNva2lsNGw1N2V2bWRleHMydjBjYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/JIX9t2j0ZTN9S/giphy.gif",
+      //     },
+      //   },
+      // ],
     };
 
     const response = await fetch(this.discordWebHookURL, {
